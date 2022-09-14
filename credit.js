4b4c8990e13592429fa164e1eb211e0bb5b87597
KemalDurukanMERT <kdurukanmert@gmail.com>
2022-09-15 00:57:13 +0200

    finish

--- credit.js
+++ credit.js
@@ -1,21 +1,22 @@
 console.log("Calculate the Credit");
-const type = document.querySelector(".type-of-credit");
-const term = document.querySelector(".term");
-const amount = document.querySelector(".amount");
-const button = document.querySelector("button");
+const type = document.querySelector("#type-of-credit");
+const term = document.querySelector("#term");
+const amount = document.querySelector("#amount");
+const button = document.querySelector(".calculate-btn");
 let rate = 0;
 let payment = 0;
 
-button.addEventListener("click", () => {
-  if (type.value === "house-loan") {
-    oran = 1.29;
-  } else if (type.value === "consumer-loan") {
-    oran = 1.99;
-  } else if (type.value === "car-loan") {
-    oran = 1.79;
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (type.value === "House Loan") {
+    rate = 1.29;
+  } else if (type.value === "Consumer Loan") {
+    rate = 1.99;
+  } else if (type.value === "Car Loan") {
+    rate = 1.79;
   }
   if (!type.value || !term.value || !amount.value) {
-    alert("Please fill the all blanks!");
+    window.alert("Please fill the all blanks!");
   }
 
   const charge_interest = rate / 100;
@@ -27,8 +28,29 @@ button.addEventListener("click", () => {
 });
 
 const getResults = () => {
-  const results = document.querySelector(".results");
+  const result = document.querySelector(".results");
+  console.log(result);
 
-  results.innerHTML =
-    `<h2>Credit Information</h2> <table> <tr><th>Amount of Credit</th><td>${amount.value}</td><th>Credit type</th><td>${type.value}</td></tr><tr><th>Term</th><td>${term.value}</td><th>Rate of Charge Interest</th><td>${rate.value}</td></tr><tr><th>Total Amount</th><td>${(payment*term.value).toFixed(2)} ₺</td><th>Amount of Payment</th><td>${payment.toFixed(2)} ₺</td></tr></table>`;
+  result.innerHTML = 
+  `<h2>Credit Information</h2>
+     <table> 
+      <tr>
+        <th>Amount of Credit</th>
+        <td>${amount.value}</td>
+        <th>Credit type</th>
+        <td>${type.value}</td>
+      </tr>
+      <tr>
+        <th>Term</th>
+        <td>${term.value}</td>
+        <th>Rate of Charge Interest</th>
+        <td>${rate}</td>
+      </tr>
+      <tr>
+        <th>Total Amount</th>
+        <td>${(payment * (term.value)).toFixed(2)} ₺</td>
+        <th>Amount of Payment</th>
+        <td>${payment.toFixed(2)} ₺</td>
+      </tr>
+    </table>`;
 };
